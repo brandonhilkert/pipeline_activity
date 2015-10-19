827201ebfb6bb022e2285039202cc9cf39ec5bb6
Brandon Hilkert <brandonhilkert@gmail.com>
2015-10-19 09:24:24 -0400

    Look at right ENV var

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var redisUrl = process.env.REDIS_URL || "127.0.0.1";
+var redisUrl = process.env.MESSAGE_BUS_REDIS_URL || "127.0.0.1";
 var redis = require("redis"),
     redisClient = redis.createClient(6379, redisUrl);
 
